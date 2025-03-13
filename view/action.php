cd6e14df525a8dbaada3d94195614a4b3becbcc0
Sayed Nafisur Rahman Alif <143423207+SayedAliff@users.noreply.github.com>
2025-03-13 09:51:17 +0600

    first commit

---
+++ view/action.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+
+<h1>Congratulation your Admin Registration is complete  !!!!!</h1>
+</body>
+</html>
---
+++ view/admin_reg.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-Pharmacy admin reg
+    </title>
+</head>
+
+<body>
+  <h1> E-Pharmacy </h1>
+  <hr>
+  <h2> Admin Registration </h2>
+
+    <form action="action.php" method="post">
+  <table>
+  <h4> Personal Informations  </h4>
+    <tr>
+        <td>Full Name: </td><td> <input type="text" name="Full Name "></td>
+        <tr>
+             <td>Date of birth: </td> <td><input type="date" id="birthday" name="birthday"></td>
+        <tr>
+        <td>Select a gender:</td>
+            <td><input type="radio" id="gender1" name="gender" value="male">Male</td>
+            <td><input type="radio" id="gender2" name="gender" value="female">Female</td>
+            <td><input type="radio" id="gender3" name="gender" value="Other">Other</td>
+        <tr>
+         <td> Phone Number: </td><td> <input type="tel" id="phone" name="phone" placeholder="+880-1* **** **** " > </td>
+
+         <tr>
+            <td>Address: </td> <td><input type="text" id="address" name="address"></td>
+         <tr>  
+        <td>Email Address: </td><td><input type="email" name="email"></td>
+        
+    <tr>
+        <td> Upload Your Image: </td><td> <input type="file" name="images" >
+    </td></tr>
+    <tr> <td><h4> Education  </h4></td>
+
+    <tr>
+        <td>Degree: </td><td> <input type="text" name="degree "></td>
+     <tr>
+        <td>Institute: </td><td> <input type="text" name="institute "></td>
+    <tr>
+        <td>Passing year: </td><td> <input type="text" name="year "></td>  
+    <tr>
+        <td><h5>Skills</h5></td>
+        <td><input type="checkbox" id="check1" name="check1" value="Inventory Management">Inventory
+        <input type="checkbox" id="check2" name="check2" value="Customer Service">Customer Support
+        <input type="checkbox" id="check3" name="check3" value="Regulatory Compliance">Compliance
+        <input type="checkbox" id="check4" name="check4" value="E-Commerce.">E-Com </td>
+
+    <tr>
+
+     <td><input type="button" onclick="alert('If you agree to follow prescription policies, payment terms, privacy rules, and legal guidelines then press Submit !!!!')" value="Terms and Conditions"></td> 
+    <tr>
+        <td> <input type="reset" name="reset" value="Reset" >
+     <input type="submit" name="submit" value="Submit" ></td>
+    </tr>
+   </table>
+  </form>
+ </body>
+</html>
