fba5244a4e8eba5b24bcdb27bb2ccbe4ff4d99c4
Sayed Nafisur Rahman Alif <143423207+SayedAliff@users.noreply.github.com>
2025-03-13 10:11:54 +0600

    2nd commit

---
+++ view/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-Pharmacy
+    </title>
+</head>
+
+<body>
+  <h1> E-Pharmacy </h1>
+  <hr>
+    <form>
+  <table>
+    <tr>
+      <td>Username:</td> <td><input type="text" name="username"></td>
+    <tr>
+      <td>Password: </td> <td><input type="password" name="password"></td>
+    <tr><td>
+    </td></tr>
+    <td><input type="submit" name="submit" value="Login" ></td>
+  </tr>
+    <tr>
+      <td><a href="http://localhost/springwt/view/admin_reg.php">Click for the Registration </a></td>
+  </table>
+</form>
+
+
+</body>
+
+
+</html>
