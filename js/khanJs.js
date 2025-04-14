8ffe666b64ff7444a3826eb83814c8769fc47076
Md Maruful Islam Khan <mdmarufulislamkhan1@gmail.com>
2025-04-14 19:27:21 +0600

    khan 1

---
+++ js/khanJs.js
@@ -0,0 +1,18 @@
+function validateName() {
+    let fname = document.getElementById("fname").value;
+    if (fname == "" || fname.length < 5) {
+        document.getElementById("error").innerHTML = "Please enter a valid name (minimum 5 characters)";
+        return false;
+    }
+    return true;
+}
+
+function validate() {
+    if (
+        !validateName() 
+       ) 
+    {
+        return false;
+    }
+    return true;
+}
